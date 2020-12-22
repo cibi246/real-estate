99571fdd1b5b3676afb4b49eb8b4134da8d43f6d
Manivannan <cibi246@gmail.com>
2020-12-22 21:21:55 +0530

    Initial commit & data file for house list

---
+++ src/data/data.js
@@ -0,0 +1,142 @@
+export const houseList = [
+  {
+    title: "Newly remoulded Mid-century home",
+    images: [
+      "https://images.unsplash.com/photo-1586112905224-08579f4dcd0b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+    ],
+    brand: "houses",
+    price: 2693.4407990587074,
+    size: "2200 sqft",
+    rooms: "2",
+    location: "Vancouver",
+    id: 0,
+    description:
+      "Whether looking for a new home for yourself or an investment opportunity, this home is an excellent choice for you! With office commercial and residential zoning, this duplex has plenty of options. ",
+  },
+  {
+    title: "Bright and Spacious Starter home",
+    images: [
+      "https://images.unsplash.com/photo-1588202783359-6078dc5093d1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+    ],
+    brand: "houses",
+    price: 2693.4407990587074,
+    size: "2200 sqft",
+    rooms: "2",
+    location: "Montreal",
+    id: 0,
+    description:
+      "Excellent core prime Richmond Row location. Yellow brick only one block from Victoria Park.",
+  },
+  {
+    title: "Cozy bungalow in the Hollywood hills",
+    images: [
+      "https://images.unsplash.com/photo-1526940946820-a61d0f92194e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+    ],
+    brand: "houses",
+    price: 2693.4407990587074,
+    size: "2200 sqft",
+    rooms: "2",
+    location: "Toronto",
+    id: 0,
+    description:
+      "Serious Investors. Welcome to this rare 6 plex in downtown London in the Woodfield area.",
+  },
+  {
+    title: "Prestigious estate with sea views",
+    images: [
+      "https://images.unsplash.com/photo-1544160464-5865101aa0d9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+    ],
+    brand: "houses",
+    price: 2693.4407990587074,
+    size: "2200 sqft",
+    rooms: "2",
+    location: "Quebec",
+    id: 0,
+    description:
+      "Excellent core prime Richmond Row location. Yellow brick only one block from Victoria Park",
+  },
+  {
+    title: "House with contemporary Loft",
+    images: [
+      "https://images.unsplash.com/photo-1562714421-aed3dcaedb62?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+    ],
+    brand: "houses",
+    price: 2693.4407990587074,
+    size: "2200 sqft",
+    rooms: "2",
+    location: "Ottawa",
+    id: 0,
+    description:
+      "Serious Investors. Welcome to this rare 6 plex in downtown London in the Woodfield area",
+  },
+  {
+    title: "Historic house",
+    images: [
+      "https://images.unsplash.com/photo-1570690732090-275b8807dd76?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ],
+    brand: "houses",
+    price: 2693.4407990587074,
+    size: "2200 sqft",
+    rooms: "2",
+    location: "Calgary",
+    id: 0,
+    description:
+      "This property is suitable for builders, investors, and business owners. This location is in proximity to downtown, and has a view of London's downtown core. ",
+  },
+  {
+    title: "Bright Hollywood writer's Bungalow",
+    images: [
+      "https://images.unsplash.com/photo-1583381707566-5e3062289547?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+    ],
+    brand: "houses",
+    price: 2693.4407990587074,
+    size: "2200 sqft",
+    rooms: "2",
+    location: "Alberta",
+    id: 0,
+    description:
+      "Amazing development site opportunity in up and coming SOHO. Ideal corner location with lots of exposure on Wellington and Simcoe Streets.",
+  },
+  {
+    title: "French Normandy-Inspired Luxury Home",
+    images: [
+      "https://images.unsplash.com/photo-1579283776054-35e8e6f86637?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+    ],
+    brand: "houses",
+    price: 2693.4407990587074,
+    size: "2200 sqft",
+    rooms: "2",
+    location: "Vancouver",
+    id: 0,
+    description:
+      "Located in Londons award-winning Woodfield Heritage District, this lovingly cared for 1912 Tudor Revival home, exudes charm and character of yesteryear. Plenty of room for everyone with 4390 sq. ft. of finished living space on 4 floors, large principal rooms, 5 bedrooms, 3 full bathrooms, 2 half baths and 4 fireplaces!",
+  },
+  {
+    title: "Mid-Century Modern Stunner Overlooking Sunset Strip",
+    images: [
+      "https://images.unsplash.com/photo-1541789643148-d4608d6b2805?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+    ],
+    brand: "houses",
+    price: 2693.4407990587074,
+    size: "2200 sqft",
+    rooms: "2",
+    location: "Montreal",
+    id: 0,
+    description:
+      "LOTS OF CHARACTER... Situated in one of London's most sought after areas is this renovated 4 bedroom / 3 bath yellow brick beauty with private side drive & detached 2 car garage.",
+  },
+  {
+    title: "Newly remoulded Mid-century home",
+    images: [
+      "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ],
+    brand: "houses",
+    price: 2693.4407990587074,
+    size: "2200 sqft",
+    rooms: "2",
+    location: "Toronto",
+    id: 0,
+    description:
+      "Absolutely fabulous INCOME PROPERTY! Completely re-done since 2018!",
+  },
+];
